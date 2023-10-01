0b48cd438a894fc0e713eb3387599c83a3a073a4
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-09-30 23:25:32 -0400

    config for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://jeremiah-carlson.github.io',
+  base: '/frigg',
+  integrations: [tailwind()],
 });
