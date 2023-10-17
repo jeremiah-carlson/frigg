c8536c90222e8bbb3ca1a59b40020b03d190455d
jeremiah-carlson <carlsonjeremiah2@gmail.com>
2023-10-16 23:35:51 -0400

    Test solidjs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jeremiah-carlson.github.io',
   base: '/frigg',
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()]
 });
---
+++ src/components/solid/Counter.jsx
@@ -0,0 +1,14 @@
+import { createSignal, onCleanup } from "solid-js";
+
+
+const Counter = () => {
+	const [count, setCount] = createSignal(0);
+	const interval = setInterval(
+		() => setCount(c => c + 1),
+		1000
+	);
+	onCleanup(() => clearInterval(interval));
+	return <div>Count value is {count()}</div>;
+};
+
+export default Counter;
